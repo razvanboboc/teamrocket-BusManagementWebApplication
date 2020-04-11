31b6547aba83b908cef0520733831e7bc5710c94
_APD_ <apauldavid@yahoo.ro>
2020-04-11 19:08:20 +0300

    Revert "Merge pull request #1 from razvanboboc/paul"
    This reverts commit 2c23756cc09fb875c63d09d51fdd8a8a9c4b1380, reversing
    changes made to d40f52f80c5486a4812de15991c29a8d121a9cb5.

--- BusCompanyManagementApplication/BusCompanyManagement.ApplicationLogic/DataModel/User.cs
+++ BusCompanyManagementApplication/BusCompanyManagement.ApplicationLogic/DataModel/User.cs
@@ -7,10 +7,10 @@ namespace BusCompanyManagement.ApplicationLogic.DataModel
     public class User
     {
         public Guid UserId { get; set; }
-        //test Paul
+
         public string EmailAdress { get; set; }
         public string Password { get; set; }
-        
+
         public string DateOfBirth { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
