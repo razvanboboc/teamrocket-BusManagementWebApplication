46f69ef667cfdcb24abef2502f0956c0384a6f57
RazvanMariusBoboc <rzvn11@gmail.com>
2020-04-12 15:58:21 +0300

    Deleted unnecessary comments

--- BusCompanyManagementApplication/BusCompanyManagement.ApplicationLogic/DataModel/User.cs
+++ BusCompanyManagementApplication/BusCompanyManagement.ApplicationLogic/DataModel/User.cs
@@ -10,7 +10,7 @@ namespace BusCompanyManagement.ApplicationLogic.DataModel
         //test Paul
         public string EmailAdress { get; set; }
         public string Password { get; set; }
-        
+
         public string DateOfBirth { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
